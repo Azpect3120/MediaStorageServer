43aa7af7526d6feefa9636ae3cc64d5ef38218bb
Azpect3120 <hhargreaves2006@gmail.com>
2023-11-06 20:13:16 -0700

    UPDATE: Created folders and images route files

---
+++ internal/routes/folders.go
@@ -0,0 +1,23 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+// Creates a folder
+func CreateFolder (ctx *gin.Context) {
+
+}
+
+// Gets a folder
+func GetFolder (ctx *gin.Context) {
+
+}
+
+// Updates a folder
+func UpdateFolder (ctx *gin.Context) {
+
+}
+
+// Deletes a folder
+func DeleteFolder (ctx *gin.Context) {
+
+}
---
+++ internal/routes/images.go
@@ -0,0 +1,23 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+// Creates a image
+func CreateImage (ctx *gin.Context) {
+
+}
+
+// Gets a image
+func GetImage (ctx *gin.Context) {
+
+}
+
+// Updates a image
+func UpdateImage (ctx *gin.Context) {
+
+}
+
+// Deletes a image
+func DeleteImage (ctx *gin.Context) {
+
+}
