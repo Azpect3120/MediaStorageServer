f75d87ac52a0c4e9f6581966070d16e758c58415
Azpect3120 <hhargreaves2006@gmail.com>
2023-11-20 17:53:06 -0700

    Added code examples in the /docs directory.

---
+++ docs/react/example.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function FileUploadComponent() {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    uploadFile = () => {
+        if (!file) {
+            console.error("No file selected.");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("media_upload", file);
+
+        fetch("<host_url>/images/<folder_id>", {
+            method: "POST",
+            body: formData,
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+    };
+
+    return (
+        <>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={uploadFile}> Upload </button>
+        </>
+    );
+}
+
+export default FileUploadComponent;
